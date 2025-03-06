8d33e20bed4a1706cda449612688d5e371aa17eb
Lea ANDRE <aleaandre@gmail.com>
2025-03-06 21:21:05 +0100

    Front test : handle cobertura format

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -28,7 +28,11 @@ module.exports = function (config) {
     coverageReporter: {
       dir: require("path").join(__dirname, "./coverage/bobapp"),
       subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      reporters: [
+        { type: "html" },
+        { type: "text-summary" },
+        { type: "cobertura" },
+      ],
     },
     reporters: ["progress", "kjhtml", "junit"],
     junitReporter: {
